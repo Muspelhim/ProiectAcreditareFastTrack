171c0c66acdf37fe628293159d4a9fd1257419c2
Robert.Csete <robertcsete@yahoo.com>
2022-06-22 00:31:04 +0300

    Exercitiu 5

--- src/test/java/org/fasttrrack/features/search/AdminTest.java
+++ src/test/java/org/fasttrrack/features/search/AdminTest.java
@@ -1,20 +1,16 @@
 package org.fasttrrack.features.search;
 
-import net.serenitybdd.junit.runners.SerenityRunner;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
 
 
 public class AdminTest extends BaseTest {
   @Test
     public void loginAsAdmin(){
-      adminSteps.loginAdmin();
+      adminSteps.doAdminLogin();
   }
   @Test
   public void verifyAdminLoggedIn(){
-    adminSteps.loginAdmin();
+    adminSteps.doAdminLogin();
     adminSteps.verifyAdminPage();
   }
 
--- src/test/java/org/fasttrrack/steps/serenity/AdminSteps.java
+++ src/test/java/org/fasttrrack/steps/serenity/AdminSteps.java
@@ -6,15 +6,16 @@ import org.fasttrrack.pages.AdminPage;
 import org.fasttrrack.pages.HomePage;
 import org.fasttrrack.pages.MyAccountPage;
 import org.fasttrrack.pages.ShopPage;
-import org.fasttrrack.utils.Constants;
 
 public class AdminSteps extends ScenarioSteps {
     private AdminPage adminPage;
     private HomePage homePage;
     private MyAccountPage myAccountPage;
     private ShopPage shopPage;
+
+
     @Step
-    public void loginAdmin(){
+    public void doAdminLogin(){
         adminPage.open();
         adminPage.completeUserAdmin();
         adminPage.completePasswordAdmin();
